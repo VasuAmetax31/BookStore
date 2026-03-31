e46232ddca4ebb011582368027d3f0469722e7e1
Vasu Ameta <himanshu2562001@gmail.com>
2026-04-01 00:22:22 +0530

    We have just setup the Navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navebar from './components/Navbar.jsx'
+import Banner from './components/Banner.jsx'
+function App() {
+  return (
+    <div>
+      <Navebar/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default App
